daeac92369a195adaf46a39f6f0d3e7e5891c01a
jal76 <jal76@googlemail.com>
2016-04-22 14:39:48 +0200

    new unit script for Ju87g and ki51

--- scripts/air_shareddefs.h
+++ scripts/air_shareddefs.h
@@ -1,6 +1,6 @@
-#define FearLimit	10 //max amount of fear they can possibly get. any additional is ignored
+#define FearLimit	12 //for JU87g and Ki51
 #define RecoverConstant 1  //how much fear they subtract from the total each RecoverRate
 #define RecoverRate	1000 //how often RecoverConstant is subtracted from total fear level
 
 //various levels of fear added when these weapons hit
-#define AAFear		2
+#define AAFear		2 / for JU87g
